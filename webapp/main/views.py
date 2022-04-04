c0fadf942aede13b82530af704ea9254a59f4a45
Jan Hopkins <janhopkins1994@hotmail.com>
2022-04-04 15:15:02 +0100

    fixed dead links main

--- webapp/main/views.py
+++ webapp/main/views.py
@@ -4,7 +4,7 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def home(request):
-    return render(request, "base_site/home.html", {"user1": request.user})
+    return render(request, "main/home.html", {"user": request.user})
     
 
 def about(request):
