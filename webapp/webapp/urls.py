b20a96fef735d78516c469c3a84fdcd13519152c
Jan Hopkins <janhopkins1994@hotmail.com>
2022-04-04 14:50:37 +0100

    migrated base code from test stack

--- webapp/webapp/urls.py
+++ webapp/webapp/urls.py
@@ -14,8 +14,13 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from base_site.views import home, about
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', home, name="home"),
+    path('about', about, name="about"),
+    path('skill_check/', include('skill_check.urls')),
+    path('accounts/', include('accounts.urls')),
 ]
