9792cf3fa232556d6a43f275faeb4c1ee0e85a0b
Jan Hopkins <janhopkins1994@hotmail.com>
2022-04-04 15:32:05 +0100

    refactored webapp

--- webapp/webapp/urls.py
+++ webapp/webapp/urls.py
@@ -15,12 +15,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-#from base_site.views import home, about
+from main.views import home, about
 
 urlpatterns = [
-    #path('admin/', admin.site.urls),
-    #path('', home, name="home"),
-    #path('about', about, name="about"),
-    #path('skill_check/', include('skill_check.urls')),
-    #path('accounts/', include('accounts.urls')),
+    path('admin/', admin.site.urls),
+    path('', include('main.urls')),
+    path('skill_matrix/', include('skill_matrix.urls')),
+    path('accounts/', include('accounts.urls')),
 ]
