f25b9c1f1cb026f7a77fde78ca34fc5c8244d425
Jan Hopkins <janhopkins1994@hotmail.com>
2022-04-04 15:12:55 +0100

    implemented default site layout

--- webapp/main/views.py
+++ webapp/main/views.py
@@ -1,3 +1,11 @@
+from email import message
+from django.http import HttpResponse
 from django.shortcuts import render
+from django.http import HttpResponse
 
-# Create your views here.
+def home(request):
+    return render(request, "base_site/home.html", {"user1": request.user})
+    
+
+def about(request):
+    return HttpResponse("This is an about page")
